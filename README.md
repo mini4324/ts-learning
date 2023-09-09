1c14cc3adebd03cd8a11670285344b39232f8c5f
nerikeshi-k <nerikeshik@gmail.com>
2023-09-09 12:58:25 +0900

    ひな形

---
+++ README.md
@@ -0,0 +1,9 @@
+# js-practice
+
+vitestのin-source testingを使ったJS練習問題集を作る試み
+
+問題をだんだん増やせたら増やす。破綻したらやめる
+
+# 使い方
+- 自分のGitHubにフォークして、解答できたらコミットしてpushする
+- `npm run test` だと全部の問題に対してテストが走ってしまうので、解いた問題にだけ走るように `npm run test src/001_simplest_add.ts` のように直指定して実行する。相対パスはvscodeを使っているなら右クリック相対パスをコピーでできる
---
+++ src/001_simplest_add.ts
@@ -0,0 +1,15 @@
+/*
+ * テストが通るように実装をしてください
+ * 以下の関数は2つの数値を受け取り、その合計を返す関数です
+ */
+export function addTwoNumbers() {}
+
+/* 以下はテストコードなので触らないでください */
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+  it('addTwoNumbers', () => {
+    expect(addTwoNumbers(1, 1)).toBe(2);
+    expect(addTwoNumbers(1, 0)).toBe(1);
+    expect(addTwoNumbers(123, 123)).toBe(246);
+  });
+}
