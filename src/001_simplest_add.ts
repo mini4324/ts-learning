a06bb48e355e33183f9c329249ff3f25fd3653e1
mini4324 <mini4.324324@gmail.com>
2023-09-10 20:19:26 +0900

    001をといた

--- src/001_simplest_add.ts
+++ src/001_simplest_add.ts
@@ -2,12 +2,14 @@
  * テストが通るように実装をしてください
  * 以下の関数は2つの数値を受け取り、その合計を返す関数です
  */
-export function addTwoNumbers() {}
+export function addTwoNumbers(a: number, b: number) {
+  return a + b;
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  it('addTwoNumbers', () => {
+  it("addTwoNumbers", () => {
     expect(addTwoNumbers(1, 1)).toBe(2);
     expect(addTwoNumbers(1, 0)).toBe(1);
     expect(addTwoNumbers(123, 123)).toBe(246);
