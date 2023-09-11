2ce0dd2a67461ceb360b3a1456f9fdf8a04641c9
mini4324 <mini4.324324@gmail.com>
2023-09-11 11:09:44 +0900

    003をといた

--- src/003_times.ts
+++ src/003_times.ts
@@ -1,12 +1,16 @@
+import { N } from "vitest/dist/reporters-cb94c88b";
+
 /*
  * 二つの数値を受け取り、その積を返す関数
  */
-export function times() {}
+export function times(a: number, b: number): number {
+  return a * b;
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  it('times', () => {
+  it("times", () => {
     expect(times(2, 1)).toBe(2);
     expect(times(1, 0)).toBe(0);
     expect(times(3, 7)).toBe(21);
