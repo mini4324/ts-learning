cabdcb3c0e2b920f53c4fb2f7c4da162f4959afc
mini4324 <mini4.324324@gmail.com>
2023-09-11 12:40:34 +0900

    007をといた

--- src/007_get_even_or_odd.ts
+++ src/007_get_even_or_odd.ts
@@ -1,16 +1,22 @@
 /*
  * 数値を一つ受け取り、偶数なら "even"、奇数なら "odd"を返す関数
  */
-export function getEvenOrOdd() {}
+export function getEvenOrOdd(a: number) {
+  if (a % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  it('getEvenOrOdd', () => {
-    expect(getEvenOrOdd(0)).toBe('even');
-    expect(getEvenOrOdd(2)).toBe('even');
-    expect(getEvenOrOdd(3)).toBe('odd');
-    expect(getEvenOrOdd(128)).toBe('even');
-    expect(getEvenOrOdd(131)).toBe('odd');
+  it("getEvenOrOdd", () => {
+    expect(getEvenOrOdd(0)).toBe("even");
+    expect(getEvenOrOdd(2)).toBe("even");
+    expect(getEvenOrOdd(3)).toBe("odd");
+    expect(getEvenOrOdd(128)).toBe("even");
+    expect(getEvenOrOdd(131)).toBe("odd");
   });
 }
