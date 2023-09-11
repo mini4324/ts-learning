553e76e8f48199c39b18e5914e6cbb1cbd708e45
mini4324 <mini4.324324@gmail.com>
2023-09-11 11:36:58 +0900

    005をといた

--- src/005_divides_int.ts
+++ src/005_divides_int.ts
@@ -2,12 +2,14 @@
  * 二つの数値を受け取り、その商を返す関数
  * ただし、商が整数でない場合は小数点以下を切り捨てる
  */
-export function devidesInt() {}
+export function devidesInt(a: number, b: number): number {
+  return Math.floor(a / b);
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  it('devidesInt', () => {
+  it("devidesInt", () => {
     expect(devidesInt(6, 2)).toBe(3);
     expect(devidesInt(12, 5)).toBe(2);
     expect(devidesInt(-15, 2)).toBe(-8);
