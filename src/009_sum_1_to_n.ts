b26d4c84d916037ab498b2e31c501011c62b27dc
mini4324 <mini4.324324@gmail.com>
2023-09-12 00:23:26 +0900

    009をといた

--- src/009_sum_1_to_n.ts
+++ src/009_sum_1_to_n.ts
@@ -1,12 +1,25 @@
 /*
  * 数値を一つ受け取り、1からその数値までの合計を返す関数
  */
-export function sumOneToN() {}
+/*export function sumOneToN(a: number) {
+  let total = 0;
+  total = ((1 + a) * a) / 2;
+  return total;
+}*/
+/*上は公式の感じで思いついたもの*/
+
+export function sumOneToN(a: number) {
+  let total = 0;
+  for (let b = 0; b < a; b++) {
+    total += b + 1;
+  }
+  return total;
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  it('sumOneToN', () => {
+  it("sumOneToN", () => {
     expect(sumOneToN(3)).toBe(6);
     expect(sumOneToN(10)).toBe(55);
   });
