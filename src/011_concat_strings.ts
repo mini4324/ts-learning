7c87bf8685ca19764626702bc70a76cce6f663e0
mini4324 <mini4.324324@gmail.com>
2023-09-12 11:47:21 +0900

    011をといた

--- src/011_concat_strings.ts
+++ src/011_concat_strings.ts
@@ -1,13 +1,17 @@
+import { S } from "vitest/dist/reporters-cb94c88b";
+
 /*
  * 文字列を二つ受け取り、それらを結合した文字列を返す関数を作成してください。
  */
-export function concatStrings() {}
+export function concatStrings(a: string, b: string): string {
+  return a + b;
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  it('concatStrings', () => {
-    expect(concatStrings('hello, ', 'world!')).toBe('hello, world!');
-    expect(concatStrings('str', 'ing')).toBe('string');
+  it("concatStrings", () => {
+    expect(concatStrings("hello, ", "world!")).toBe("hello, world!");
+    expect(concatStrings("str", "ing")).toBe("string");
   });
 }
