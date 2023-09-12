74917827150f1b74a5c527c1c9729f72820b3121
mini4324 <mini4.324324@gmail.com>
2023-09-12 11:49:53 +0900

    012をといた

--- src/012_capitalize.ts
+++ src/012_capitalize.ts
@@ -2,14 +2,16 @@
  * 文字列を一つ受け取り、小文字を全て大文字にして返す関数を作成してください。
  * たとえば、'hello world' を渡したら 'HELLO WORLD' を返します。
  */
-export function capitalize() {}
+export function capitalize(a: string): string {
+  return a.toUpperCase();
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  it('capitalize', () => {
-    expect(capitalize('string')).toBe('STRING');
-    expect(capitalize('hello world')).toBe('HELLO WORLD');
-    expect(capitalize('HeLlO WoRlD!')).toBe(capitalize('hElLo wOrLd!'));
+  it("capitalize", () => {
+    expect(capitalize("string")).toBe("STRING");
+    expect(capitalize("hello world")).toBe("HELLO WORLD");
+    expect(capitalize("HeLlO WoRlD!")).toBe(capitalize("hElLo wOrLd!"));
   });
 }
