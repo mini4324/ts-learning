7a860287d14ee7e87500a91cac581aa7a784c825
mini4324 <mini4.324324@gmail.com>
2023-09-17 21:52:24 +0900

    017をといた

--- src/017_reverse_word.ts
+++ src/017_reverse_word.ts
@@ -1,10 +1,14 @@
+import { WorkerRPC } from "vitest";
+
 /*
  * 文字列を受け取り、逆さまにして返す関数。
  * "hello" -> "olleh"
  * "hello, world!" -> "!dlrow ,olleh"
  * "こんにちは" -> "はちにんこ"
  */
-export function reverseWord() {}
+export function reverseWord(word: string) {
+  return word.split("").reverse().join("");
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
