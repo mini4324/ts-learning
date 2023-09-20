e8f8f8ed4b0469e99fe93a57b5f1a62b7f2fdf0f
mini4324 <mini4.324324@gmail.com>
2023-09-20 16:07:20 +0900

    020をといた

--- src/020_omit_value.ts
+++ src/020_omit_value.ts
@@ -2,7 +2,13 @@
  * 数値の配列arrayと数値nを受け取る。arrayからnを取り除いたような配列を返す。
  * たとえば、渡された引数が[1, 1, 2, 3, 1, 5, 8]と1なら、[2, 3, 5, 8]が返る。
  */
-function omitValue() {}
+function omitValue(a: Array<number>, n: number) {
+  const result = a.filter(function (b) {
+    return b !== n;
+  });
+
+  return result;
+}
 
 /* 以下はテストコードなので触らないでください */
 if (import.meta.vitest) {
